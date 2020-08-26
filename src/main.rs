85344a47472c39a206212af451ab78194af88954
Aryan Regmi <**>
2020-08-26 17:02:06 -0600

    Implement C1,C2,C3 Euler rotations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use ndarray::prelude::*;
+
+struct Degrees(f64);
+
+fn C1(theta: Degrees) -> Array2<f64> {
+    let rad = theta.0.to_radians();
+    let a1 = array![[1., 0., 0.],
+                                                [0., rad.cos(), rad.sin()],
+                                                [0., -(rad.sin()), rad.cos()]];
+    
+    a1
+}
+
+fn C2(theta: Degrees) -> Array2<f64> {
+    let rad = theta.0.to_radians();
+    let a1 = array![[rad.cos(), 0., -(rad.sin())],
+                                                [0., 1., 0.],
+                                                [rad.sin(), 0., rad.cos()]];
+    
+    a1
+}
+
+fn C3(theta: Degrees) -> Array2<f64> {
+    let rad = theta.0.to_radians();
+    let a1 = array![[rad.cos(), rad.sin(), 0.],
+                                                [-(rad.sin()), rad.cos(), 0.],
+                                                [0., 0., 1.]];
+    
+    a1
+}
+
 fn main() {
-    println!("Hello, world!");
+    let a = C1(Degrees(30.));
+    println!("\n{:#?}\n", a);
+
+    let b = C2(Degrees(30.));
+    println!("\n{:#?}\n", b);
+
+    let c = C3(Degrees(30.));
+    println!("\n{:#?}\n", c);
 }
