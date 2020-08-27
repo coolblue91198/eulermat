6baf24596fd9149df1ec2fd51867291f0b1ede61
Aryan Regmi <**>
2020-08-27 12:55:36 -0600

    Add custom floating point precision

--- src/main.rs
+++ src/main.rs
@@ -2,40 +2,49 @@ use ndarray::prelude::*;
 
 struct Degrees(f64);
 
+
+fn float_precision(x: f64) -> f64{
+    (x * 100.0).round() / 100.0
+}
+
 fn C1(theta: Degrees) -> Array2<f64> {
     let rad = theta.0.to_radians();
     let a1 = array![[1., 0., 0.],
-                                                [0., rad.cos(), rad.sin()],
-                                                [0., -(rad.sin()), rad.cos()]];
+                                                [0., float_precision(rad.cos()), float_precision(rad.sin())],
+                                                [0., float_precision(-(rad.sin())), float_precision(rad.cos())]];
     
     a1
 }
 
 fn C2(theta: Degrees) -> Array2<f64> {
     let rad = theta.0.to_radians();
-    let a1 = array![[rad.cos(), 0., -(rad.sin())],
+    let a1 = array![[float_precision(rad.cos()), 0., float_precision(-(rad.sin()))],
                                                 [0., 1., 0.],
-                                                [rad.sin(), 0., rad.cos()]];
+                                                [float_precision(rad.sin()), 0., float_precision(rad.cos())]];
     
     a1
 }
 
 fn C3(theta: Degrees) -> Array2<f64> {
     let rad = theta.0.to_radians();
-    let a1 = array![[rad.cos(), rad.sin(), 0.],
-                                                [-(rad.sin()), rad.cos(), 0.],
+    let a1 = array![[float_precision(rad.cos()), float_precision(rad.sin()), 0.],
+                                                [float_precision(-(rad.sin())), float_precision(rad.cos()), 0.],
                                                 [0., 0., 1.]];
     
     a1
 }
 
 fn main() {
-    let a = C1(Degrees(30.));
-    println!("\n{:#?}\n", a);
+    // let a = C1(Degrees(30.));
+    // println!("\n{:#?}\n", a);
 
-    let b = C2(Degrees(30.));
-    println!("\n{:#?}\n", b);
+    // let b = C2(Degrees(30.));
+    // println!("\n{:#?}\n", b);
 
-    let c = C3(Degrees(30.));
-    println!("\n{:#?}\n", c);
+    // let c = C3(Degrees(30.));
+    // println!("\n{:#?}\n", c);
+
+    let sol = C2(Degrees(-90.)) * C1(Degrees(90.));
+
+    println!("{:#?}", sol);
 }
