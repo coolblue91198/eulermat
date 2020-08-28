add11341cbca5465ab3e46f82fa5dc6ea322f77f
Aryan Regmi <**>
2020-08-28 13:02:33 -0600

    Added command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,33 @@
 mod matrix_helpers;
-use matrix_helpers::{C1, C2, C3, Degrees};
+use matrix_helpers::{C1, C2, C3, Degrees, RotMatrix};
+use std::env;
 
-fn main() {
-    let theta = &Degrees(35.);
+fn main() -> Result<(), String> {
+    let args: Vec<String> = env::args().collect();
 
-    (C1(*theta) * C2(*theta) * C3(*theta)).print();
+    let val: Option<RotMatrix> = match &args[1][0..] {
+        "c1" => {
+            let angle = args[2].parse::<f64>().unwrap_or(0.);
+
+            Some(C1(Degrees(angle)))
+        },
+
+        &_ => None 
+    };
+
+    match val {
+        None => Err(String::from("Please enter Ci(angle) -  Where i is 1, 2, or 3 and angle is a floating point.")),
+
+        Some(mat) => {
+            mat.print();
+            Ok(())
+        }
+    }
+
+    // Ok(())
+
+    // let theta = &Degrees(35.);
+
+    // (C1(*theta) * C2(*t"heta) * C3(*theta)).print();
 
 }
--- src/matrix_helpers/mod.rs
+++ src/matrix_helpers/mod.rs
@@ -2,3 +2,4 @@ mod rotational_matrix;
 mod euler_angles;
 
 pub use euler_angles::{C1, C2, C3, Degrees};
+pub use rotational_matrix::{RotMatrix};
