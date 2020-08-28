88b4cf8f2403e050c375f6527151e07675dfe33c
Aryan Regmi <**>
2020-08-28 15:08:24 -0600

    Improved error handling + added TODO items

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,16 @@ mod matrix_helpers;
 use matrix_helpers::{C1, C2, C3, Degrees, RotMatrix};
 use std::env;
 
-fn main() {
+fn main() -> Result<(), String> {
     let args: Vec<String> = env::args().collect();
 
     let num_args = args.len() - 1;
 
-    if num_args % 2 == 0 {
+    if (num_args % 2 == 0) & (num_args > 0) {
         let mut results:Vec<RotMatrix> = Vec::new();
 
         for i in 1..num_args {
-            let val = match &args[i].to_lowercase()[0..] {
+            let arg = match &args[i].to_lowercase()[0..] {
                 "c1" => {
                     let angle = args[i+1].parse::<f64>().unwrap_or(0.);
                     Some(C1(Degrees(angle)))
@@ -30,25 +30,42 @@ fn main() {
                 &_ => None
             };
             
-            match val {
+            match arg {
                 Some(mat) => results.push(mat),
                 None => ()
             }
         }
 
-        let res_len = results.len() - 1;
+        let mut res_len = results.len();
+        if res_len > 0 {
+            res_len = res_len - 1;
+        }
 
-        match res_len > 0 {
+        let val = match res_len >= 0 {
             true => {
                 for i in 0..results.len()-1 {
                     results[i+1] = &results[i] * &results[i+1];
                 }
-                println!("{:}", res_len);
-                results[res_len].print();                      
+                results[res_len].print();
+                Some(())
             },
 
-            false => {println!("ITS FALSE!!")}
+            false => { None }
+        };
+        
+        match val {
+            Some(()) => Ok(()),
+            None => Err(String::from("\nInvalid Use! Pass rotations as separate, ordered arguments: C1(30.), C2(20.), etc.\n"))
         }
+
+    }
+    else {
+        Err(String::from("\nInvalid Use! Pass rotations as separate, ordered arguments: C1(30.), C2(20.), etc.\n"))
     }   
 
 }
+
+// TODO: Route Errors to stderr instead of stdout
+// TODO: Write logic in separate file ?
+// TODO: Write tests
+// TODO: Double check error handling
